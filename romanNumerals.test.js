97e11ea1e149b60e6ec89bb2402d5e0074e4aeb3
krist <kristoffer.b.fernholt@gmail.com>
2025-08-26 13:50:58 +0200

    second commit

--- romanNumerals.test.js
+++ romanNumerals.test.js
@@ -1,9 +1,19 @@
 import { test, expect } from "vitest";
 
-function romanNumerals() {
-  return "I";
+function romanNumerals(number) {
+  let answer = "";
+  if (number === 1) {
+    answer = "I";
+  } else {
+    answer = "II";
+  }
+  return answer;
 }
 
 test("1 in roman numerals is I", () => {
   expect(romanNumerals(1)).toBe("I");
+});
+
+test("2 in roman numerals is II", () => {
+  expect(romanNumerals(2)).toBe("II");
 });
